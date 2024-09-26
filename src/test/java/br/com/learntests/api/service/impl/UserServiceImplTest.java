5c4f75a289b54d42b649be55f04ac59bf5ed7002
oLucasNascimento <lucasf.ferreira@bandtec.com.br>
2024-09-26 16:58:59 -0300

    criado os testes para a classe de servicos

--- src/test/java/br/com/learntests/api/service/impl/UserServiceImplTest.java
+++ src/test/java/br/com/learntests/api/service/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.learntests.api.service.impl;
 import br.com.learntests.api.domain.User;
 import br.com.learntests.api.domain.dto.UserDTO;
 import br.com.learntests.api.repository.UserRepository;
+import br.com.learntests.api.service.exception.DataIntegratyViolationException;
 import br.com.learntests.api.service.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
@@ -25,6 +27,7 @@ class UserServiceImplTest {
     public static final String NAME = "Lucas";
     public static final String MAIL = "luquinha@mail.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NÃO_ENCONTRADO = "Objeto não encontrado";
 
     @InjectMocks
     private UserServiceImpl service;
@@ -47,7 +50,7 @@ class UserServiceImplTest {
 
     @Test
     void whenFindByIdThenReturnAnUserInstance() {
-        when(this.repository.findById(Mockito.anyInt())).thenReturn(optionalUser);
+        when(this.repository.findById(Mockito.anyInt())).thenReturn(this.optionalUser);
         User response = this.service.findById(ID);
 
         assertNotNull(response);
@@ -59,30 +62,109 @@ class UserServiceImplTest {
     }
 
     @Test
-    void whenFindByIdThenReturnAnObjectNotFoundException(){
-        when(this.repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
-        try{
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(this.repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NÃO_ENCONTRADO));
+        try {
             this.service.findById(ID);
-        }catch (Exception ex){
+        } catch (Exception ex) {
             assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(OBJETO_NÃO_ENCONTRADO, ex.getMessage());
         }
     }
 
     @Test
-    void findAll() {
+    void whenFindAllThenReturnAnListOfUsers() {
+
+        when(this.service.findAll()).thenReturn(List.of(this.user));
+
+        List<User> users = this.service.findAll();
+
+        assertNotNull(users);
+        assertEquals(1, users.size());
+        assertEquals(User.class, users.get(0).getClass());
+
+        assertEquals(ID, users.get(0).getId());
+        assertEquals(NAME, users.get(0).getName());
+        assertEquals(MAIL, users.get(0).getEmail());
+        assertEquals(PASSWORD, users.get(0).getPassword());
+
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSucess() {
+        when(this.repository.save(any())).thenReturn(this.user);
+
+        User response = this.service.create(this.userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(MAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
-    void update() {
+    void whenCreateThenReturnAnDataViolationException() {
+        when(this.repository.findByEmail(anyString())).thenReturn(this.optionalUser);
+
+        try {
+            optionalUser.get().setId(2);
+            this.service.create(userDTO);
+        } catch (Exception ex){
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("Email já cadastrado no sistema", ex.getMessage());
+        }
     }
 
     @Test
-    void delete() {
+    void whenUpdateThenReturnSucess() {
+        when(this.repository.save(any())).thenReturn(this.user);
+
+        User response = this.service.update(this.userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(MAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
+    }
+
+    @Test
+    void whenUpdateThenReturnAnDataViolationException() {
+        when(this.repository.findByEmail(anyString())).thenReturn(this.optionalUser);
+
+        try {
+            optionalUser.get().setId(2);
+            this.service.update(userDTO);
+        } catch (Exception ex){
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("Email já cadastrado no sistema", ex.getMessage());
+        }
+    }
+
+    @Test
+    void deleteWithSuccess() {
+        when(this.repository.findById(anyInt())).thenReturn(this.optionalUser);
+        doNothing().when(this.repository).deleteById(anyInt());
+
+        this.service.delete(ID);
+        verify(this.repository, times(1)).deleteById(anyInt());
+    }
+
+    @Test
+    void deleteWithObjectNotFoundException(){
+        when(this.repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NÃO_ENCONTRADO));
+
+        try{
+           this.service.delete(ID);
+        } catch (Exception ex){
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJETO_NÃO_ENCONTRADO, ex.getMessage());
+        }
+
     }
 
     private void startUser() {
